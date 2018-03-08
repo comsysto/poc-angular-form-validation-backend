a446f5a1db7eac9a3caef7f89f9d1c1d9ee4a09e
clouless <github@gruenewaldt.net>
2018-03-08 18:30:45 +0100

    ticket validation

--- api/tickets.ts
+++ api/tickets.ts
@@ -4,17 +4,47 @@
  */
 import { Router } from 'express';
 import * as path from 'path';
+import { isNullOrUndefined } from 'util';
+import { Ticket, ValidationError } from '../model/api.model';
 export const ticketsRouter = Router();
+
+
+
+//
+// VALIDATION
+//
+const validateCreateTicket = (ticket: Ticket, i18n: Function): ValidationError[] => {
+  const errors: ValidationError[] = [];
+  if (!ticket.hasOwnProperty('id') || isNullOrUndefined(ticket.id)) {
+    errors.push({ 
+      field: 'id',
+      type: 'required',
+      i18n: 'validation.ticket.id.required',
+      message: i18n('validation.ticket.id.required')
+    })
+  }
+  return errors;
+}
 
 //
 // ROUTES /api/tickets
 //
-// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language
-
 ticketsRouter.get('/', (req, res, next) => {
   res.send('ok').status(200)
 });
 
 ticketsRouter.post('/', (req, res, next) => {
-  res.status(204).json({ ok: 'ok' });
+  const errors = validateCreateTicket(req.body, req.i18n);
+  if (errors.length > 0) {
+    res.status(420).json({
+      status: 420,
+      message: 'invalid ticket',
+      errors: errors,
+    });
+  } else {
+    res.status(204).json({
+      status: 200,
+      message: 'ok' 
+    });
+  }
 });
--- model/api.model.ts
+++ model/api.model.ts
@@ -1,8 +1,21 @@
+/*
+ * @license MIT
+ * Copyright (c) 2017 - 2018 Bernhard Grünewaldt
+ */
 export class Ticket {
   constructor(
     public id: string,
     public summary: string,
     public reporter: string,
     public description?: string,
+  ) {}
+}
+
+export class ValidationError {
+  constructor(
+    public field: string,
+    public type: string,
+    public i18n: string,
+    public message: string,
   ) {}
 }
